55056c3e349a557d1d1e7cbeaa59d58dd81e34de
dmitrii <pecherkin.dmitrii@gmail.com>
2026-06-19 17:39:27 +0300

    hw1 initial

--- hw02_unpack_string/go.mod
+++ hw02_unpack_string/go.mod
@@ -1,6 +1,6 @@
-module github.com/fixme_my_friend/hw02_unpack_string
+module github.com/pecherkin-dmitrii/otus-hw/hw02_unpack_string
 
-go 1.23
+go 1.26
 
 require github.com/stretchr/testify v1.10.0
 
--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,38 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"regexp"
+	"strconv"
+	"strings"
+	"unicode/utf8"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+var startsWithDigits = regexp.MustCompile(`^\d+`)
+var containsNumber = regexp.MustCompile(`\d{2,}`)
+
+func Unpack(str string) (string, error) {
+	switch {
+	case utf8.RuneCountInString(str) == 0:
+		return "", nil
+	case startsWithDigits.MatchString(str) || containsNumber.MatchString(str):
+		return "", ErrInvalidString
+	default:
+		builder := strings.Builder{}
+		runeSlice := []rune(str)
+		for i, ch := range runeSlice {
+			if _, err := strconv.Atoi(string(ch)); err == nil {
+				continue
+			} else if i+1 == len(runeSlice) {
+				builder.WriteRune(ch)
+			} else if digit, err := strconv.Atoi(string(runeSlice[i+1])); err != nil {
+				builder.WriteRune(ch)
+				continue
+			} else {
+				builder.WriteString(strings.Repeat(string(ch), digit))
+			}
+		}
+		return builder.String(), nil
+	}
 }
