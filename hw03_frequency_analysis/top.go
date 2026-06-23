e0bff26f3dd64ca04796904ee4cd18f8582cb94d
dmitrii <pecherkin.dmitrii@gmail.com>
2026-06-23 15:18:29 +0300

    hw3 initial

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,50 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"cmp"
+	"slices"
+	"strings"
+)
+
+const amountOfTopWordsToTake = 10
+
+type wordWithCount struct {
+	word  string
+	count int
+}
+
+var wordWithCountComparator = func(a, b wordWithCount) int {
+	if n := cmp.Compare(b.count, a.count); n != 0 {
+		return n
+	}
+	return cmp.Compare(a.word, b.word)
+}
+
+func Top10(str string) []string {
+	splitted := strings.Fields(str)
+
+	wordsMap := make(map[string]int)
+	for _, word := range splitted {
+		wordsMap[word]++
+	}
+
+	wordsSlice := make([]wordWithCount, 0, len(wordsMap))
+	for word, count := range wordsMap {
+		wordWithCount := wordWithCount{
+			word:  word,
+			count: count,
+		}
+		wordsSlice = append(wordsSlice, wordWithCount)
+	}
+	slices.SortStableFunc(wordsSlice, wordWithCountComparator)
+
+	result := make([]string, 0, amountOfTopWordsToTake)
+	for i, w := range wordsSlice {
+		if i == cap(result) {
+			break
+		}
+		result = append(result, w.word)
+	}
+
+	return result
 }
