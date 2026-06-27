f5e58d659735f1684836f548a78aae9b4cb1418f
dmitrii <pecherkin.dmitrii@gmail.com>
2026-06-27 14:58:00 +0300

    hw3 added test cases

--- hw03_frequency_analysis/top_test.go
+++ hw03_frequency_analysis/top_test.go
@@ -43,6 +43,23 @@ var text = `Как видите, он  спускается  по  лестни�
 	посидеть у огня и послушать какую-нибудь интересную сказку.
 		В этот вечер...`
 
+var textEng = ` Formatting issues are the most contentious but the
+	least consequential. People can adapt to different formatting styles
+	but it's better if they don't have to, and less time is devoted to
+	the topic if everyone adheres to the same style. The problem is how
+	to approach this Utopia without a long prescriptive style guide.
+	
+	With Go we take an unusual approach and let the machine take care of
+	most formatting issues. The gofmt program (also available as go fmt,
+	which operates at the package level rather than source file level)
+	reads a Go program and emits the source in a standard style of
+	indentation and vertical alignment, retaining and if necessary
+	reformatting comments. If you want to know how to handle some new
+	layout situation, run gofmt; if the answer doesn't seem right, 
+	rearrange your program (or file a bug about gofmt), don't work around it. `
+
+var textSmall = "  Formatting   issues   are  the  most contentious  but the"
+
 func TestTop10(t *testing.T) {
 	t.Run("no words in empty string", func(t *testing.T) {
 		require.Len(t, Top10(""), 0)
@@ -78,5 +95,34 @@ func TestTop10(t *testing.T) {
 			}
 			require.Equal(t, expected, Top10(text))
 		}
+	})
+
+	t.Run("positive test with English text", func(t *testing.T) {
+		expected := []string{
+			"the",      // 8
+			"to",       // 6
+			"and",      // 5
+			"a",        // 4
+			"if",       // 4
+			"program",  // 3
+			"Go",       // 2
+			"The",      // 2
+			"approach", // 2
+			"but",      // 2
+		}
+		require.Equal(t, expected, Top10(textEng))
+	})
+
+	t.Run("positive test with small text", func(t *testing.T) {
+		expected := []string{
+			"the",         // 2
+			"Formatting",  // 1
+			"are",         // 1
+			"but",         // 1
+			"contentious", // 1
+			"issues",      // 1
+			"most",        // 1
+		}
+		require.Equal(t, expected, Top10(textSmall))
 	})
 }
